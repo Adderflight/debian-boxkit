45bacd829f917baf1065269441e4fe3b94b6dd43
adderflight <adderflight1@protonmail.com>
2025-11-11 20:30:35 -0500

    replace cosign pubkey, change container file, and add scripts

---
+++ 1-retire-box.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# RUN THIS FIRST
+
+# remove chromium browser export
+#distrobox-export -a chromium-browser -d
+
+# remove binaries exports
+#distrobox-export -b /usr/bin/nmap -d
+#distrobox-export -b /usr/bin/httrack -d
+#distrobox-export -b /usr/bin/wine -d
---
+++ 2-setup-box.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# export chromium browser
+#distrobox-export -a chromium-browser
+
+# export binaries
+#distrobox-export -b /usr/bin/nmap -ep /home/adderflight/.local/bin
+#distrobox-export -b /usr/bin/httrack -ep /home/adderflight/.local/bin
+
+# install wine and export it
+#sudo dnf install -y wine
+
+#distrobox-export -b /usr/bin/wine -ep /home/adderflight/.local/bin
